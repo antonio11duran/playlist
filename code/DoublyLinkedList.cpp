3c2c32e4249346b0f60a9836a7573cec59695ef7
Antonio Duran <andu9805@colorado.edu>
2024-12-09 23:29:47 +0000

    double linked list backend complete & tests completed

--- code/DoublyLinkedList.cpp
+++ code/DoublyLinkedList.cpp
@@ -6,11 +6,99 @@ DoublyLL::DoublyLL() {
 
 DoublyLL::~DoublyLL() {}
 
-song* init_song(string title, string artist) {
+song* DoublyLL::init_song(string title, string artist) {
     song* new_song(new song);
     new_song->artist = artist;
     new_song->title = title;
     return new_song;
+}
+
+string DoublyLL::display_playlist() {
+    string playlist;
+    song* cursor = first_song;
+    if (cursor == NULL) {
+        return "";    
+    }
+    while (cursor != NULL) {
+        playlist.append(cursor->title).append(" by ").append(cursor->artist);
+        playlist.append("\n");
+        cursor = cursor->next;
+    }
+    return playlist;
+}
+
+string DoublyLL::current_song() {
+    return "";
+}
+
+song* DoublyLL::next_song(song* current_song) {
+    return current_song->next;
+}
+
+song* DoublyLL::prev_song(song* current_song) {
+    return current_song->prev;
+}
+
+void DoublyLL::add_song(string title, string artist) {
+    song* new_song = init_song(title, artist);
+    song* cursor = first_song;
+    if (cursor == NULL) {
+        first_song = new_song;
+        return;
+    }
+    while (cursor->next != NULL) {
+        cursor = cursor->next;
+    }
+    cursor->next = new_song;
+    cursor->next->prev = cursor;
+}
+
+song* DoublyLL::find_song(string title, string artist) {
+    song* cursor = first_song;
+    if (cursor == NULL) {
+        return NULL;
+    }
+    while (cursor != NULL) {
+        if (cursor->title.compare(title) == 0) {
+            if (cursor->artist.compare(artist) == 0) {
+                return cursor;
+            }
+        }
+        cursor = cursor->next;
+    }
+    return NULL;
+}
+
+void DoublyLL::remove_song(song* deleted_song) {
+    song* cursor = deleted_song;
+    if (cursor == NULL) {
+        return;
+    }
+    song* before = prev_song(deleted_song);
+    song* after = next_song(deleted_song);
+    deleted_song->next = NULL;
+    deleted_song->prev = NULL;
+    if (before == NULL && after == NULL) {
+        set_top(NULL);
+        return;
+    }
+    if (before == NULL) {
+        after->prev = NULL;
+        deleted_song->next = NULL;
+        set_top(after);
+        return;
+    }
+    if (after == NULL) {
+        before->next = NULL;
+        deleted_song->prev = NULL;
+        return;
+    }
+    before->next = after;
+    after->prev = before;
+}
+
+void DoublyLL::set_top(song* first_song_) {
+    first_song = first_song_;
 }
 
 song* DoublyLL::get_first_song() { return first_song; }
--- code/DoublyLinkedList.h
+++ code/DoublyLinkedList.h
@@ -1,3 +1,4 @@
+#include <memory>
 #include <string>
 
 using namespace std;
@@ -11,8 +12,10 @@ struct song {
 
 class DoublyLL {
 public:
+    // constructor
     DoublyLL();
 
+    // deconstructor
     ~DoublyLL();
 
     song* init_song(string title, string artist);
@@ -21,15 +24,19 @@ public:
 
     string current_song(); // might need to switch to a private cursor?
 
-    song* next_song();
+    song* next_song(song* current_song);
 
-    song* prev_song();
+    song* prev_song(song* current_song);
 
-    void add_song();
+    void add_song(string title, string artist);
 
-    void remove_song();
+    song* find_song(string title, string artist);
+
+    void remove_song(song* deleted_song);
 
     song* get_first_song();
+
+    void set_top(song* first_song_);
 private:
     song* first_song;
 };
